baed79739af4b82c3f4d5db13e2b5d89a92ef694
Kue Yang <kzy1@zips.uakron.edu>
2016-11-25 13:15:27 -0500

    Moved SPI related hardware configuration into a seperate module.

---
+++ Senior_Design_Project.X/HardwareProfile.h
@@ -0,0 +1,21 @@
+/**
+ * @file HardwareProfile.h
+ * @author Kue Yang
+ * @date 11/22/2016
+ * @brief Defines System Configurations.
+ */
+
+#ifndef _HARDWAREPROFILE_H_
+#define _HARDWAREPROFILE_H_
+
+/**@def SYS_FREQ 
+ * Defines the System Clock Speed. */
+#define SYS_FREQ                (40000000L)    // 40MHz
+/**@def GetPeripheralClock() 
+ * Calculates and returns the Peripheral Clock Speed. */
+#define	GetPeripheralClock()    (SYS_FREQ/(1 << OSCCONbits.PBDIV))
+/**@def GetInstructionClock() 
+ * Calculates and returns the Instruction Clock Speed. */
+#define	GetInstructionClock()	(SYS_FREQ)
+
+#endif
