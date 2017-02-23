96c9a92ac460b9252ac3b5dd55589a27491b723a
Kue Yang <kzy1@zips.uakron.edu>
2017-02-22 23:21:13 -0500

    Added nate's algorithm to adc interrupt service routine.

--- Senior_Design_Project.X/ADC.c
+++ Senior_Design_Project.X/ADC.c
@@ -9,10 +9,26 @@
 /**@def NUM_OF_ADCCHANNELS Defines the number of ADC channels used. */
 #define NUM_OF_ADCCHANNELS      1
 #define ADC_BUF_SIZE            128
+#define ADC_MID_SCALE           1<<5
+#define ADC_MIN_THREADHOLD      10
+#define ADC_ARRAY_SIZE          5
 
 BYTE adcData[ADC_BUF_SIZE];
 int adcBufIndex;
 BOOL isStrumming;
+
+BOOL isPositive;
+UINT16 previousValue;
+
+// Stores values for a single peak
+UINT16 peakMax[ADC_ARRAY_SIZE];
+UINT16 peakMin[ADC_ARRAY_SIZE];
+
+// Stores values for different peaks
+UINT16 localMax[ADC_ARRAY_SIZE];
+UINT16 localMin[ADC_ARRAY_SIZE];
+
+void ADC_ZeroBuffer(void);
 
 /**
  * @brief Initializes the ADC module.
@@ -55,8 +71,11 @@ void ADC_Init(void)
     INTSetVectorPriority(INT_ADC_VECTOR, INT_PRIORITY_LEVEL_2);
     INTSetVectorSubPriority(INT_ADC_VECTOR, INT_SUB_PRIORITY_LEVEL_1);
     
-    adcBufIndex = 0;
+    // Initializes strumming variables
+    ADC_ZeroBuffer();
     isStrumming = FALSE;
+    isPositive = FALSE;
+    previousValue = 0;
 }
 
 /**
@@ -68,6 +87,21 @@ void ADC_Process(void)
     
 }
 
+void ADC_ZeroBuffer(void)
+{
+    adcBufIndex = 0;
+    
+    peakMax[0] = ADC_MID_SCALE; peakMax[1] = ADC_MID_SCALE; peakMax[2] = ADC_MID_SCALE;
+    peakMax[3] = ADC_MID_SCALE; peakMax[4] = ADC_MID_SCALE; 
+    peakMin[0] = ADC_MID_SCALE; peakMin[1] = ADC_MID_SCALE; peakMin[2] = ADC_MID_SCALE;
+    peakMin[3] = ADC_MID_SCALE; peakMin[4] = ADC_MID_SCALE; 
+    
+    localMax[0] = ADC_MID_SCALE; localMax[1] = ADC_MID_SCALE; localMax[2] = ADC_MID_SCALE;
+    localMax[3] = ADC_MID_SCALE; localMax[4] = ADC_MID_SCALE; 
+    localMin[0] = ADC_MID_SCALE; localMin[1] = ADC_MID_SCALE; localMin[2] = ADC_MID_SCALE;
+    localMin[3] = ADC_MID_SCALE; localMin[4] = ADC_MID_SCALE; 
+}
+
 /**
  * @brief ADC Interrupt Service Routine.
  * @details The interrupt service routine is used read the strummer sensor.
@@ -77,19 +111,93 @@ void __ISR(_ADC_VECTOR, IPL2AUTO) ADCHandler(void)
 {
     // Reads the ADC buffer
     adcData[adcBufIndex] = (WORD)ADC1BUF0;
-    
-    // Checks if strumming occur, strumming occurs if the voltage read is non-zero
-    if(adcData[adcBufIndex++] > 10)
+    if(adcData[adcBufIndex] >= ADC_MID_SCALE)
     {
-//        IO_scanFrets();                             // Scans for the fret press.
-//        AUDIO_setNewTone(IO_getCurrentFret());      // Sets the file to be read.
-//        if(!TIMER3_IsON())
-//        {
-//            TIMER3_ON(TRUE);    // Kick starts reading the audio file process.
-//        }
+        isPositive = TRUE;
+    }
+    else
+    {
+        isPositive = FALSE;
     }
     
-    if(adcBufIndex >= ADC_BUF_SIZE)
+    // Stores the biggest sample read by magnitude
+    if(isPositive && (adcData[adcBufIndex] > peakMax[0]))
+    {
+        peakMax[4] = peakMax[3];
+        peakMax[3] = peakMax[2]; 
+        peakMax[2] = peakMax[1];
+        peakMax[1] = peakMax[0]; 
+        peakMax[0] = adcData[adcBufIndex];
+    }
+    else if(!isPositive && (adcData[adcBufIndex] < peakMin[0]))
+    {
+        peakMin[4] = peakMin[3];
+        peakMin[3] = peakMin[2]; 
+        peakMin[2] = peakMin[1];
+        peakMin[1] = peakMin[0]; 
+        peakMin[0] = adcData[adcBufIndex];
+    }
+    
+    // Checks if sample changes polarity
+    if(!isPositive && (previousValue > ADC_MID_SCALE))
+    {
+        // Takes the average of the local max
+		localMax[4] = localMax[3];
+		localMax[3] = localMax[2];
+		localMax[2] = localMax[1];
+		localMax[1] = localMax[0];
+        localMax[0] = (peakMax[4] + peakMax[3] + peakMax[2] + peakMax[1] + peakMax[0])/5;
+        UINT16 tempMax = (localMax[4] + localMax[3] + localMax[2] + localMax[1])/4;
+        
+        // Compares local maxs to determine if user has strum.
+        if(localMax[0] > tempMax)
+        {
+            IO_scanFrets();                             // Scans for the fret press.
+            AUDIO_setNewTone(IO_getCurrentFret());      // Sets the file to be read.
+            if(!TIMER3_IsON())
+            {
+                TIMER3_ON(TRUE);                        // Kick starts reading the audio file process.
+            }
+        }
+        else if((localMax[0] - ADC_MID_SCALE) < ADC_MIN_THREADHOLD)
+        {
+            if(TIMER3_IsON())
+            {
+                TIMER3_ON(FALSE);                        // Stops the audio file process.
+            }
+        }
+    }
+    else if(isPositive && (previousValue < ADC_MID_SCALE))
+    {
+        localMin[4] = localMin[3];
+		localMin[3] = localMin[2];
+		localMin[2] = localMin[1];
+		localMin[1] = localMin[0];
+        localMin[0] = (peakMin[4] + peakMin[3] + peakMin[2] + peakMin[1] + peakMin[0])/5;
+        UINT16 tempMin = (localMin[4] + localMin[3] + localMin[2] + localMin[1])/5;
+        
+        // Compares local mins to determine if user has strum.
+        if(localMin[0] > tempMin)
+        {
+            IO_scanFrets();                             // Scans for the fret press.
+            AUDIO_setNewTone(IO_getCurrentFret());      // Sets the file to be read.
+            if(!TIMER3_IsON())
+            {
+                TIMER3_ON(TRUE);                        // Kick starts reading the audio file process.
+            }
+        }
+        else if((localMin[0] - ADC_MID_SCALE) < ADC_MIN_THREADHOLD)
+        {
+            if(TIMER3_IsON())
+            {
+                TIMER3_ON(FALSE);                        // Stops the audio file process.
+            }
+        }
+    }
+    
+    previousValue = adcData[adcBufIndex];   // Stores the previous read value.
+    
+    if((adcBufIndex++) >= ADC_BUF_SIZE)
     {
         adcBufIndex = 0;
     }
