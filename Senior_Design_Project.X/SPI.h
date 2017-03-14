e0e3f04dd6d972ae45edd761e582942c0eb2eecf
Kue Yang <kzy1@zips.uakron.edu>
2017-03-13 20:38:24 -0400

    Fixed SPI function prototypes.

--- Senior_Design_Project.X/SPI.h
+++ Senior_Design_Project.X/SPI.h
@@ -13,19 +13,8 @@ extern "C" {
     
 void SPI_Init(void);
 BYTE SPI2_ReadWrite(BYTE);
-
-/**@def SD_CS SD Chip Select Output bit. */
-#define SD_CS               LATBbits.LATB7
-/**@def SD_CS_TRIS SD Chip Select TRIS bit. */
-#define SD_CS_TRIS          TRISBbits.TRISB7
-
-/**@def SD_CS_TRIS Description: SD-SPI Card Detect Input bit */
-#define SD_CD               PORTBbits.RB8
-/**@def SD_CS_TRIS Description: SD-SPI Card Detect TRIS bit */
-#define SD_CD_TRIS          TRISBbits.TRISB8
-
-void SPI1_Init(int clk);
-BYTE SPI1_ReadWrite(BYTE ch);
+void SPI3_Init(int clk);
+BYTE SPI3_ReadWrite(BYTE ch);
 
 #ifdef	__cplusplus
 }
